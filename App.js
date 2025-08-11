528d1604e33e756ebf9a516fe3f7983e2c6bfb08
Jude Almeida <jude94almeida@gmail.com>
2025-08-11 02:18:02 -0500

    refactor: replace App.js with navigation entry point

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/navigation/Tabs';
 
 export default function App() {
-  return <Tabs />;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
