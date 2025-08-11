6e308e92be991a30e7a05300e23a1059fadd89b2
Jude Almeida <jude94almeida@gmail.com>
2025-08-11 06:54:25 -0500

    chore: latest changes before debug

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/navigation/Tabs';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
-  );
+  return <Tabs />;
 }
